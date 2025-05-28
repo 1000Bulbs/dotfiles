240917d56de79839f9e7c11b7b851cb72e48a733
Fernando Alemán <fernandoaleman@mac.com>
2025-05-28 13:18:24 -0500

    feat: add bin directory to path

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,3 +1,10 @@
+# Add bin directory to the PATH
+BIN_DIR="$HOME/.bin"
+
+if [[ -d $BIN_DIR ]]; then
+  path=("$BIN_DIR" $path)
+fi
+
 # Add asdf shims to the PATH
 ASDF_SHIMS="${ASDF_DATA_DIR:-$HOME/.asdf}/shims"
 
